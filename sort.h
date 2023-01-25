4cbbff81bc64c2fc79b5987edc59cf96e6be2325
Sylva <onyekanwachokor@gmail.com>
2023-01-25 01:49:58 +0100

    Temp Sorting

--- sort.h
+++ sort.h
@@ -25,4 +25,14 @@ void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+
 #endif
